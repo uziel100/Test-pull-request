266eefe40a544434a4ae537a213af37a1ffc0942
Uziel Gonzalez <20181138@uthh.edu.mx>
2021-09-17 23:32:44 -0500

    new partner added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 const teamContainer = document.querySelector('#team');
-        const team = [
-            { 
-                name: 'Uziel Meliton Hernandez',
-                email: 'uzielmelitonh@gmail.com'                
-            }
-        ]
+const team = [
+    {
+        name: 'Uziel Meliton Hernandez',
+        email: 'uzielmelitonh@gmail.com'
+    },
+    {
+        name: 'Yony Garcia',
+        email: '20181138@uthh.edu.mx'
+    }
+]
 
-        
-        const render = () => {
-            let content = '';
-            team.forEach(item => {
-                return content += `<li>${ item.name } - ${  item.email }</li>`   
-            })
 
-            teamContainer.innerHTML = content;
-        }
+const render = () => {
+    let content = '';
+    team.forEach(item => {
+        return content += `<li>${item.name} - ${item.email}</li>`
+    })
 
-        render();
+    teamContainer.innerHTML = content;
+}
+
+render();
